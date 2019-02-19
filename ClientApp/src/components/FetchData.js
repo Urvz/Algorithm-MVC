fedf35d06b88a64cef72293f4afe574c3f2ff606
Urvz <urvapatel1@gmail.com>
2019-02-19 11:56:19 -0500

    mvc using c# and react to show sorting algorithms

---
+++ ClientApp/src/components/FetchData.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+
+export class FetchData extends Component {
+	static displayName = FetchData.name;
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			userArray: [],
+			sort: 0,	//sort type 0=merge sort, 1=quick sort, 2=bubble sort
+			sortType: 'Merge Sort',
+			input: 0,	//input type
+			inputType: 'Int',
+			result: 'Press submit to see your sorted Array here!',  //result array
+			comment: '' //details of the submission
+
+		};	
+		this.handleTextChange = this.handleTextChange.bind(this);  //bind all methods to prevent looping
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.sortMethod = this.sortMethod.bind(this);
+	}
+
+	componentDidMount() {
+		console.log('This is my submission of the MAPSTED assessment. A few comments: Input value can take 500 single digit values(for now).I tried to use !this.state.userArray.some(isNaN) to see if the input is truely just integers, but I was getting an error saying some is not a function, with more time I could solve this. I was unable to account for double, int or string in my method. I tried to bugfix with using dynamic instead but changing int-long values did not let that happen so I ran out of time. I was also unable to show the steps due to the methods in the surver overwritting my jagged array. I would love to talk about these issues with you, other than that everything else is working fine! Thank you for considering me and this project will be a great addition to my portfolio.');
+	}
+
+	sortMethod(option, sort) {	//updates state for sort type
+		this.setState({ sort: option, sortType: sort }, () => {
+			console.log(this.state.sort + " " + this.state.sortType);
+		});
+	}
+
+	inputMethod(option, input) {	//updates state for input type
+		this.setState({ input: option, inputType: input }, () => {
+			console.log(this.state.input + " " + this.state.inputType);
+		});
+	}
+
+	handleTextChange(e) {	//updates userarray value
+		this.setState({ userArray: e.target.value });
+	}
+
+	handleSubmit(e) {	//sends a POST request to the server
+		e.preventDefault();
+		//if (!this.state.userArray.some(isNaN)) {
+			const userArray = JSON.parse("[" + this.state.userArray + "]");
+		//}
+		const sort = this.state.sort;
+		const input = this.state.input;
+		const inputType = this.state.inputType;
+		console.log("now user input" + userArray)
+		// TODO: send request to the server
+		fetch('api/SortAlgorithm/Searchs', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json, text/plain, */*',
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify({ userArray: userArray, sort: sort, input: input })	//sending the array, method of sorting and input type
+		})
+			.then((res) => res.json())
+			.then((data) => {			//update the state after the server has finished changing the array 
+				console.log(data);
+				this.setState({ result: data.inputs })
+				if (data.sortMethod === 0) {
+					this.setState({ comment: ' is your result using Merge Sort with input type as ' + inputType })
+				} else if (data.sortMethod === 1) {
+					this.setState({ comment: ' is your result using Quick Sort with input type as ' + inputType})
+				} else {
+					this.setState({ comment: ' is your result using Bubble Sort with input type as ' + inputType})
+				}
+			}).catch(function () {
+				this.setState({ comment: ' you have entered the array incorrectly. Please enter it like this: 4,3,2,1 '});
+			});
+	}
+
+	render() {
+
+		return (
+			<div className='text-center'>
+				<h1>Sorting Algorithms</h1>
+				<p>This component demonstrates posting a user-entered array to the server, and fetching the sorted algorithm from the server.</p>
+				<div className="dropdown">
+					<button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						{this.state.sortType}
+					</button>
+					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+						<a className="dropdown-item" onClick={() => this.sortMethod(0, "Merge Sort")}>Merge Sort</a>
+						<a className="dropdown-item" onClick={() => this.sortMethod(1, "Quick Sort")}>Quick Sort</a>
+						<a className="dropdown-item" onClick={() => this.sortMethod(2, "Bubble Sort")}>Bubble Sort</a>
+					</div>
+				</div>
+				<br />
+				<div className="dropdown">
+					<button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						{this.state.inputType}
+					</button>
+					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+						<a className="dropdown-item" onClick={() => this.inputMethod(0, "Int")}>Int</a>
+						<a className="dropdown-item" onClick={() => this.inputMethod(1, "Double")}>Double</a>
+						<a className="dropdown-item" onClick={() => this.inputMethod(2, "String")}>String</a>
+					</div>
+				</div>
+				<br />
+				<form className="form" onSubmit={this.handleSubmit}>
+					<div className="form-group ">
+						<input type="text" className="form-control" minLength="1" maxLength="999" placeholder="Enter your array here, separated by a Comma ex:1,4,2,3,6" onChange={this.handleTextChange.bind(this)} />
+					</div>
+					<button type="submit" className="btn btn-primary mb-12">Submit</button>
+				</form>
+				<h1 className="display-4">{this.state.result}  {this.state.comment}</h1>
+			</div>
+		);
+	}
+}
---
+++ Controllers/SortAlgorithmController.cs
@@ -0,0 +1,197 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace my_new_app.Controllers
+{
+    [Route("api/[controller]")]
+    public class SortAlgorithmController : Controller
+    {
+
+		[HttpPost("[action]")]
+		public Search Searchs()
+		{
+			//init variables here, I have tried using dynamic to support double, int and string 
+			//however it would not work due to types changing, will try to debug this on
+			var input = new StreamReader(Request.Body).ReadToEnd();
+			dynamic inputClone = JsonConvert.DeserializeObject(input);
+			int[] myInput = inputClone.userArray.ToObject<int[]>();
+			int[][] jaggedArray = new int[myInput.Length][];
+
+			void merge(int[] Arr, int start, int mid, int end) {
+				// create a temp array
+				int[] temp = new int[end - start + 1];
+				//int temp[end - start + 1];
+
+				// crawlers for both intervals and for temp
+				int i = start, j = mid + 1, k = 0;
+
+				// traverse both arrays and in each iteration add smaller of both elements in temp 
+				while (i <= mid && j <= end)
+				{
+					if (Arr[i] <= Arr[j])
+					{
+						temp[k] = Arr[i];
+						k += 1; i += 1;
+					}
+					else
+					{
+						temp[k] = Arr[j];
+						k += 1; j += 1;
+					}
+				}
+
+				// add elements left in the first interval 
+				while (i <= mid)
+				{
+					temp[k] = Arr[i];
+					k += 1; i += 1;
+				}
+
+				// add elements left in the second interval 
+				while (j <= end)
+				{
+					temp[k] = Arr[j];
+					k += 1; j += 1;
+				}
+
+				// copy temp to original interval
+				for (i = start; i <= end; i += 1)
+				{
+					Arr[i] = temp[i - start];
+					
+				}
+			}
+
+			// Arr is an array of integer type
+			// start and end are the starting and ending index of current interval of Arr
+
+			void mergeSort(int[] Arr, int start, int end)
+			{
+
+				if (start < end)
+				{
+					int mid = (start + end) / 2;
+					mergeSort(Arr, start, mid);
+					mergeSort(Arr, mid + 1, end);
+					merge(Arr, start, mid, end);
+				}
+			}
+
+			
+			void quickSort(int[] arr, int left, int right)
+			{
+				if (left < right)
+				{	//see if left is smaller than right to initialize pivot
+					int pivot = Partition(arr, left, right);
+
+					//check value of pivot to see if array has been partitioned correctly, then quicksort recursively
+					if (pivot > 1)
+					{
+						quickSort(arr, left, pivot - 1);
+					}
+					if (pivot + 1 < right)
+					{
+						quickSort(arr, pivot + 1, right);
+					}
+				}
+
+			}
+
+			int Partition(int[] arr, int left, int right)
+			{
+				//use base value of left aas pivot
+				int pivot = arr[left];
+				while (true)
+				{
+
+					while (arr[left] < pivot)
+					{
+						left++;
+					}
+
+					while (arr[right] > pivot)
+					{
+						right--;
+					}
+
+					//swap position of elements if the smaller one comes before the larger one
+					if (left < right)
+					{
+						int temp = arr[left];
+						arr[left] = arr[right];
+						arr[right] = temp;
+
+						if (arr[left] == arr[right])
+							left++;
+					}
+					else
+					{
+						return right;
+					}
+				}
+			}
+
+			void bubbleSort(int[] arr)
+			{
+				//I have tried using a jagged 2d array to store the steps of my sort method, however it is being overwritten..would love to chat about this
+				int temp = 0;
+				int jagged = 0;
+				
+				//simple bubble sort checking every element and comparing it to the one beside it
+				for (int write = 0; write < arr.Length; write++)
+				{
+					for (int sort = 0; sort < arr.Length - 1; sort++)
+					{
+						if (arr[sort] > arr[sort + 1])
+						{
+							jaggedArray[jagged] = arr;
+							jagged++;
+							temp = arr[sort + 1];
+							arr[sort + 1] = arr[sort];
+							arr[sort] = temp;
+						}
+					}
+				}
+			}
+
+			//use the sorting method chosed by the user in the front-end
+			if (inputClone.sort == 0)
+			{
+				mergeSort(myInput, 0, myInput.Length - 1);
+			}
+			else if (inputClone.sort == 1)
+			{
+				quickSort(myInput, 0, myInput.Length - 1);
+			}
+			else
+			{
+				bubbleSort(myInput);
+			}
+			string newArray = JsonConvert.SerializeObject(myInput);
+			int type = inputClone.sort;
+
+			//return the array, sort method(to see if correct method has been chosen), and the steps
+			return new Search
+			{
+					Inputs = newArray,
+					SortMethod = type,
+					Steps = jaggedArray
+			};
+		}
+
+		//model in the controller for simplification
+		public class Search
+		{
+			public string Inputs { get; set; }
+			public int SortMethod { get; set; }
+			public Array Steps { get; set; }
+		}
+
+		
+    }
+}
